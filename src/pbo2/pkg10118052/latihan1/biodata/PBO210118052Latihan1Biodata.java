e3a15623ccf1e773ba37ac3841bf6de455bc2e53
Bagus Syibro Malisi <55564197+syibro98@users.noreply.github.com>
2019-09-23 12:58:17 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118052/latihan1/biodata/PBO210118052Latihan1Biodata.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118052.latihan1.biodata;
+
+/**
+ * @author 
+ * NAMA      : Bagus Syibro Malisi
+ * KELAS     : PBO2
+ * NIM       : 10118052
+ * Deskripsi Program : Program ini berisi program Untuk Menampilkan value ke layar  
+ * 
+ */
+public class PBO210118052Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "---------------------");
+        System.out.println("NAMA\t\t: Bagus Syibro Malisi");
+        System.out.println("ALAMAT\t\t: Jalan IR H JUANDA No 33");
+        System.out.println("TEMPAT LAHIR\t: Indramayu");
+        System.out.println("TANGGAL LAHIR\t: 28 Mei 2000");
+        System.out.println("---------------------");
+        System.out.println("HOBI\t\t: Lari & Travelling");
+        System.out.println("MAKES\t\t: Ayam Goreng");
+        System.out.println("MIKES\t\t: Jus Buah Naga");
+    }
+    
+}
